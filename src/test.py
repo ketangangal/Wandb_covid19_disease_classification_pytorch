66446f604067375a3e5444758d6a905ab0356f3c
ketanGangal <ketangangal98@gmail.com>
2022-05-06 13:17:29 +0530

    wandb integration

--- src/test.py
+++ src/test.py
@@ -1,47 +1,87 @@
-import torch
+from src.custom_dataloader.customdata import CustomDataset
+from src.custom_exception.exception import CustomException
+from src.custom_logger.logger import CustomLogger
+from src.utils.common import read_config
 from torch.utils.data import DataLoader
-from utils.customdata import CustomDataset
 from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from from_root import from_root
+import sys
+import torch
+import wandb
 import os
 
-# Loss can be calculated 2 ways
-# Loss on single image or on batch
-def test_net(path=None):
-    try:
-        # Creating DataLoader
-        test_DataLoader = DataLoader(CustomDataset('test'), batch_size=8, shuffle=False)
-        cost = CrossEntropyLoss()
-        net = torch.load(path)
-        net.eval()
+wandb.init(job_type="experiment-1", project="Image_Classification", entity="playbook-k10", name="test-ex-1")
+logger = CustomLogger("logs")
 
-        test_loss = 0
-        test_accuracy = 0
-        total = 0
-        total_batch = 0
-        with torch.no_grad():
-            for batch, data in enumerate(test_DataLoader):
-                images = data[0].to(device)
-                labels = data[1].to(device)
 
-                output = net(images)
-                loss = cost(output, labels)
+class ModelTester:
+    def __init__(self, config):
+        self.num_epochs = config['test_parameters']['Num_Epochs']
+        self.Batch_size = config['test_parameters']['Batch_size']
+        self.learning_rate = config['test_parameters']['learning_rate']
+        self.eps = config['test_parameters']['eps']
+        self.shuffle = config['test_parameters']['shuffle']
+        self.in_channels = config['test_parameters']['in_channels']
+        self.output = config['test_parameters']['output']
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.test_loss = 0
+        self.test_accuracy = 0
+        self.total = 0
+        self.total_batch = 0
 
-                predictions = torch.argmax(output, 1)
-                print(f"Actual_Labels : {labels}     Predictions : {predictions}     labels : {loss.item():.4f}",)
+    def configuration(self):
+        try:
+            test_DataLoader = DataLoader(CustomDataset('test'), batch_size=self.Batch_size, shuffle=self.shuffle)
+            net = torch.load("./training_result/Model._2022_05_05_12_28/model._2022_05_05_12_28.pt").to(self.device)
+            cost = CrossEntropyLoss()
+            optimizer = Adam(net.parameters(), lr=self.learning_rate, eps=float(self.eps))
+            net.eval()
+            return test_DataLoader, net, cost, optimizer
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
-                test_loss += loss.item()
-                test_accuracy += (predictions == labels).sum().item()
-                total_batch +=1
-                total += labels.size(0)
+    def test_net(self):
+        try:
+            test_DataLoader, net, cost, optimizer = self.configuration()
+            with torch.no_grad():
+                holder = []
+                for batch, data in enumerate(test_DataLoader):
+                    images = data[0].to(self.device)
+                    labels = data[1].to(self.device)
 
-        print(f"Model  -->   Loss : {test_loss /total_batch }      Accuracy : {(test_accuracy/total)*100} %")
+                    output = net(images)
+                    loss = cost(output, labels)
 
-    except Exception as e:
-        raise e
+                    predictions = torch.argmax(output, 1)
+
+                    for i in zip(images, labels, predictions):
+                        h = list(i)
+                        h[0] = wandb.Image(h[0])
+                        holder.append(h)
+
+                    print(f"Actual_Labels : {labels}     Predictions : {predictions}     labels : {loss.item():.4f}", )
+
+                    self.test_loss += loss.item()
+                    self.test_accuracy += (predictions == labels).sum().item()
+                    self.total_batch += 1
+                    self.total += labels.size(0)
+
+            print(
+                f"Model  -->   Loss : {self.test_loss / self.total_batch} Accuracy : {(self.test_accuracy / self.total) * 100} %")
+            wandb.log({"Test Loss": self.test_loss / self.total_batch,
+                       "Test Accuracy": (self.test_accuracy / self.total) * 100})
+            my_table = wandb.Table(data=holder, columns=["image", "label", "class_prediction"])
+            wandb.log({"Predictions": my_table})
+
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
 
 if __name__ == '__main__':
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f'Inferencing Using Device : {device} \n')
-    path = os.path.join('.', 'training_result', 'Model._2022_03_30_12_21', 'model._2022_03_30_12_21.pt')
-    test_net(path=path)
+    config_path = os.path.join(from_root(), 'src', 'configurations', 'config.yaml')
+    config = read_config(config_path)
+    tester = ModelTester(config=config)
+    tester.test_net()
--- src/train.py
+++ src/train.py
@@ -1,75 +1,93 @@
 from src.model_structure.model import NeuralNetwork
 from src.custom_dataloader.customdata import CustomDataset
 from src.custom_exception.exception import CustomException
+from src.custom_logger.logger import CustomLogger
 from src.utils.common import read_config, save_model, save_plot
 from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
+from from_root import from_root
+import sys
 import torch
 import wandb
 import time
 import os
 
+run = wandb.init(job_type="experiment-1", project="Image_Classification", entity="playbook-k10", name="train-ex-1")
+logger = CustomLogger("logs")
+print(run.name)
 
-wandb.init(project="Image_Classification", entity="playbook-k10")
-def train_net(num_epochs=None, Batch_size=None, learning_rate=None, eps=None, shuffle=None, in_channels=None,output=None):
-    try:
-        # Creating DataLoader
-        train_DataLoader = DataLoader(CustomDataset('train'), batch_size=Batch_size, shuffle=shuffle)
 
-        # Initialization of the neuralNetwork
-        net = NeuralNetwork(in_channels, output)
-        net.to(device)
+class ModelTrainer:
+    def __init__(self, config: dict):
+        self.num_epochs = config['train_parameters']['Num_Epochs']
+        self.Batch_size = config['train_parameters']['Batch_size']
+        self.learning_rate = config['train_parameters']['learning_rate']
+        self.eps = config['train_parameters']['eps']
+        self.shuffle = config['train_parameters']['shuffle']
+        self.in_channels = config['train_parameters']['in_channels']
+        self.output = config['train_parameters']['output']
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.losses = []
+        self.accuracy = []
+        logger.info(f'Training Using Device : {self.device}')
 
-        # Initialization Of optimizer and Cost Functions
-        optimizer = Adam(net.parameters(), lr=learning_rate, eps=float(eps))
-        cost = CrossEntropyLoss()
-        wandb.watch(net,optimizer,log="all")
-        losses = []
-        accuracy = []
+    def __configuration(self):
+        try:
+            train_DataLoader = DataLoader(CustomDataset('train'), batch_size=self.Batch_size, shuffle=self.shuffle)
+            net = NeuralNetwork(self.in_channels, self.output).to(self.device)
+            cost = CrossEntropyLoss()
+            optimizer = Adam(net.parameters(), lr=self.learning_rate, eps=float(self.eps))
+            logger.info("Created Train Loader, Net, Cost, Optimizer")
+            return train_DataLoader, net, cost, optimizer
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
-        for epoch in range(num_epochs):
-            for batch, data in enumerate(train_DataLoader):
-                images = data[0].to(device)
-                labels = data[1].to(device)
-                optimizer.zero_grad()
+    def __save_artifacts(self, net):
+        try:
+            path = os.path.join('.', 'training_result', str(time.strftime("Model._%Y_%m_%d_%H_%M")))
+            if not os.path.exists(path):
+                os.mkdir(path)
+                save_plot(self.losses, self.accuracy, 'plot', path)
+                save_model(net, 'model', path)
+            logger.info(f"Saved Artifacts At {path}")
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
-                # Prediction and Loss Calculation
-                predictions = net.forward(images)
-                loss = cost(predictions, labels)
+    def train_net(self):
+        try:
+            train_DataLoader, net, cost, optimizer = self.__configuration()
+            for epoch in range(self.num_epochs):
+                for batch, data in enumerate(train_DataLoader):
+                    images = data[0].to(self.device)
+                    labels = data[1].to(self.device)
+                    optimizer.zero_grad()
 
-                # Backward propagation
-                loss.backward()
+                    predictions = net.forward(images)
+                    loss = cost(predictions, labels)
+                    loss.backward()
+                    optimizer.step()
 
-                # Weight adjustments
-                optimizer.step()
+                    self.losses.append(loss.item())
+                    # acc = (correct / total)  < -- > correct is (predictions == labels)
+                    acc = torch.mean((torch.argmax(predictions, 1) == labels).float()).item()
+                    self.accuracy.append(acc)
+                    wandb.log({"train" : {"loss": self.losses[-1], "Accuracy": self.accuracy[-1]}})
+                print(f" Epoch: {epoch}     Loss: {self.losses[-1]}     Accuracy: {self.accuracy[-1]}")
+                logger.info(f" Epoch: {epoch}     Loss: {self.losses[-1]}     Accuracy: {self.accuracy[-1]}")
 
-                losses.append(loss.item())
-                # acc = (correct / total)  < -- > correct is (predictions == labels)
-                acc = torch.mean((torch.argmax(predictions, 1) == labels).float()).item()
-                accuracy.append(acc)
-                wandb.log({"loss": losses[-1], "Accuracy": accuracy[-1]})
-            print(f" Epoch: {epoch}     Loss: {losses[-1]}     Accuracy: {accuracy[-1]}")
-        path = os.path.join('.', 'training_result', str(time.strftime("Model._%Y_%m_%d_%H_%M")))
-        if not os.path.exists(path):
-            os.mkdir(path)
-            save_plot(losses, accuracy, 'plot', path)
-            save_model(net, 'model', path)
+            self.__save_artifacts(net)
 
-        print("Model and plot Saved")
-
-    except Exception as e:
-        raise e
+            print("Model and plot Saved")
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
 
 if __name__ == '__main__':
-    config = read_config('./config.yaml')
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f'Training Using Device : {device}')
-    train_net(num_epochs=config['parameters']['Num_Epochs'],
-              Batch_size=config['parameters']['Batch_size'],
-              learning_rate=config['parameters']['learning_rate'],
-              eps=config['parameters']['eps'],
-              shuffle=config['parameters']['shuffle'],
-              in_channels=config['parameters']['in_channels'],
-              output=config['parameters']['output'])
+    config_path = os.path.join(from_root(), 'src', 'configurations', 'config.yaml')
+    config = read_config(config_path)
+    trainer = ModelTrainer(config)
+    trainer.train_net()
